89c1d337b61895a5d78fedfe28317971090438aa
Christine Shaffer <cmc.shaffer@gmail.com>
2017-11-06 16:50:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function shout (string) {
   return string.toUpperCase()
 }
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  return console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  return console.log(string.toLowerCase())
+}
